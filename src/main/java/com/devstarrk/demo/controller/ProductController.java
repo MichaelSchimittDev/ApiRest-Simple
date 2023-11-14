3da2663134ed8a8d7d58a048e9cecacf0b8c0984
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-13 21:57:23 -0300

    Finish ApiRest simple

--- src/main/java/com/devstarrk/demo/controller/ProductController.java
+++ src/main/java/com/devstarrk/demo/controller/ProductController.java
@@ -1,2 +1,28 @@
-package com.devstarrk.demo.controller;public class ProductController {
+package com.devstarrk.demo.controller;
+
+import com.devstarrk.demo.entities.Department;
+import com.devstarrk.demo.entities.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public List<Product> getObjects(){
+        Department d1 = new Department(1L, "Tech");
+        Department d2 = new Department(2L, "Pet");
+
+        Product p1 = new Product(1L, "MacBookPro", 4000.0, d1);
+        Product p2 = new Product(2L, "PcGamer", 5000.0, d1);
+        Product p3 = new Product(3L, "PetHouse", 300.0, d2);
+
+        return Arrays.asList(p1,p2,p3);
+    }
 }
--- src/main/java/com/devstarrk/demo/entities/Product.java
+++ src/main/java/com/devstarrk/demo/entities/Product.java
@@ -1,2 +1,18 @@
-package com.devstarrk.demo.entities;public class Product {
+package com.devstarrk.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Department department;
+
 }
