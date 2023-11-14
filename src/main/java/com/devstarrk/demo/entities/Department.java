774a39ff4a2155d1fda8602156e1f85b5d7df132
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-13 21:22:10 -0300

    create class department

---
+++ src/main/java/com/devstarrk/demo/entities/Department.java
@@ -0,0 +1,13 @@
+package com.devstarrk.demo.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Department {
+    private Long id;
+    private String name;
+
+
+}
